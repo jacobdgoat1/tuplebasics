0d1000e6f74691f22513a4387add5b9e6db0bb0d
Jacob Thomas <thomajac002@warren.k12.in.us>
2024-11-01 10:15:16 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Tuple Creation and indexing
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+print(fruit_tuple[3])
+slice_tuple = ([1],[2+1])
+print(slice_tuple)
+#Tuple Methods
+num_tuple = (3, 5, 3, 2, 8, 3, 7)
+print(num_tuple.count(3))
+print(num_tuple.index(8))
+#Tuple Unpacking
+person_info = ("Alice", 28, "Engineer")
+name, number, boolean = person_info
+print(name)
+print(number)
+print(boolean)
+
